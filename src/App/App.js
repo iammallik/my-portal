ef2b610ce2301705bc9fcdfed937f2a1e5f64b46
mamallik <gajji.mallikarjun@iamplus.com>
2018-04-04 16:35:29 +0530

    adding lint to project

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+//import logo from '../logo.svg';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import '../App.css';
 import AppLogo from './AppLogo';
 import Articles from './Articles';
+import Resume from './Resume';
 
 class App extends Component {
    render() {
@@ -14,12 +15,14 @@ class App extends Component {
                <ul>
                   <li><Link to={'/'}>Home</Link></li>
                   <li><Link to={'/Articles'}>Articles</Link></li>
+                  <li><Link to={'/Resume'}>Resume</Link></li>
                </ul>
                <hr />
 
                <Switch>
                   <Route exact path='/' component={AppLogo} />
                   <Route exact path='/Articles' component={Articles}/>
+                  <Route exact path='/Resume' component={Resume}/>
                </Switch>
             </div>
          </Router>
